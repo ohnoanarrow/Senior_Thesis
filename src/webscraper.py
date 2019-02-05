9e0ec50f867be3d717d08c579c5e144fbbae3cc6
Tara Douglass <douglasst@aldenv105.allegheny.edu>
2019-02-05 00:51:23 -0500

    Progress on collection of deck color

--- src/webscraper.py
+++ src/webscraper.py
@@ -1,19 +1,71 @@
 # import statements
-import urllib2
+# from urllib.request import urlopen
+# import mechanize
+import requests
 import re
-import mechanize
+from itertools import cycle
 from bs4 import BeautifulSoup
 
-# specify the url
-quote_page = 'https://mtgdecks.net/Modern/tournaments/page:2'
-# query the website and return the html to the variable page
-page = urllib2.urlopen(quote_page)
-# parse the html using beautiful soup and store in variable soup
-soup = BeautifulSoup(page, 'html.parser')
+color_dictionary = {
+    "u": "mono blue",
+    "w": "mono white",
+    "g": "mono green",
+    "r": "mono red",
+    "b": "mono black",
+    "uw": "azorius",
+    "bu": "dimir",
+    "br": "rakdos",
+    "gr": "gruul",
+    "gw": "selesnya",
+    "bw": "orzhov",
+    "ru": "izzet",
+    "bg": "golgari",
+    "rw": "boros",
+    "gu": "simic",
+    "bgr": "jund",
+    "guw": "bant",
+    "bru": "grixis",
+    "grw": "naya",
+    "buw": "esper",
+    "ruw": "jeskai",
+    "brw": "mardu",
+    "bgu": "sultai",
+    "gru": "temur",
+    "bgw": "abzan",
+    "bgru": "chaos",
+    "bgrw": "aggression",
+    "bguw": "growth",
+    "bruw": "artifice",
+    "gruw": "altruism",
+    "bgruw": "rainbow"
+}
 
+
+# specify the url
+quote_page = 'https://mtgdecks.net/Modern/modern-libourne-is-magic-4-libourne-tournament-54073'
+# query the website and return the html to the variable page
+page = requests.get(quote_page)
+# parse the html using beautiful soup and store in variable soup
+soup = BeautifulSoup(page.text, 'html.parser')
+
+# Keep these on the VERY OUTSIDE OF ALL LOOPS, THESE ARE NOT TO BE RESET
+sb_id = 0
+tourn_num = 0
 str= r"[\x2D]"
-counter = 0
-for link in soup.find_all('a', attrs={'href':re.compile(str)}):
-    if counter > 0 and counter <= 20:
-        print(link.get('href'))
-    counter += 1
+
+# Isolating the top 4 decks
+none_counter = 0
+rank_counter = 1
+
+for tag in soup.find_all('tr'):
+    # The strong tag will first come up with deck ranking
+    if tag.strong is None:
+        none_counter += 1
+    # This is where we pull the top 4 decks
+    if none_counter > 0 and none_counter < 2 and rank_counter <= 4:
+        # Making sure there is a value under the strong tag
+        if tag.strong is not None:
+            color = tag.find('div', attrs={'class':"small"})
+            for x in color.contents:
+                print(x)
+            rank_counter += 1
