0f798855b2226b40a162f506a163f556a20560c0
Tara Douglass <douglasst@allegheny.edu>
2019-04-12 00:52:35 -0400

    Working mana curve and archetype

---
+++ src/analysis/mana_archetype.py
@@ -0,0 +1,56 @@
+import sqlite3
+from mana_archetype_db import Database
+
+def mana_and_archetype(rows):
+    # Rows in this order: Color Number Mana_Cost Archetype
+    mana_array = []
+    arch_array = []
+    for row in rows:
+        color = row[0]
+        number = row[1]
+        cost = row[2]
+        archetype = row[3]
+        # The following lines check to see if both the color and mana cost are
+        # in an item in the 2d array
+        if len(mana_array) == 0:
+            mana_array.append([color,number,cost])
+
+        else:
+            count = 0
+            for i in range(len(mana_array)):
+                if color in mana_array[i]:
+                    if cost in mana_array[i]:
+                        mana_array[i][1] += number
+                        count += 1
+
+            if count == 0:
+                mana_array.append([color,number,cost])
+
+        if len(arch_array) == 0:
+            arch_array.append([color,number,archetype])
+
+        else:
+            count = 0
+            for i in range(len(arch_array)):
+                if color in arch_array[i]:
+                    if archetype in arch_array[i]:
+                        arch_array[i][1] += number
+                        count += 1
+
+            if count == 0:
+                arch_array.append([color,number,archetype])
+
+
+def main():
+    db_file = 'databases/MtG.db'
+    conn = Database.create_connection(db_file)
+    #colors=['chaos','mono green','temur','bant']
+    with conn:
+        rows = Database.man_arch(conn,'chaos')
+        mana_and_archetype(rows)
+#        for color in colors:
+#            rows = Database.man_arch(conn,color)
+#            mana_and_archetype(rows)
+
+if __name__ == '__main__':
+    main()
---
+++ src/analysis/mana_archetype_db.py
@@ -0,0 +1,38 @@
+import sys
+import sqlite3
+from sqlite3 import Error
+
+class Database:
+    def create_connection(db_file):
+        """ create a database connection to a SQLite database """
+        try:
+            conn = sqlite3.connect(db_file)
+            return conn
+        except Error as e:
+            print(e)
+
+        return None
+
+    def man_arch(conn,color):
+        """
+        Query all rows in the colors table
+        :param conn: the Connection object
+        :return:
+        """
+        arch = 'Land'
+        arch2 = (arch,)
+        color2 = (color,)
+        test = (arch,color)
+        sql = ''' SELECT Color, Number, Mana_Cost, Archetype
+                    FROM Colors
+                    INNER JOIN Cards
+                    ON Colors.Card_ID=Cards.Card_ID
+                    WHERE Archetype !=?
+                    AND Color=?'''
+
+        cur = conn.cursor()
+        cur.execute(sql,test)
+
+        rows = cur.fetchall()
+
+        return rows
